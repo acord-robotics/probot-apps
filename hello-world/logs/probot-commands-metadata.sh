a84ee61baa22d22db6ad461a8cac30229b76075b
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-10-07 11:10:55 +0800

    Create probot-commands-metadata.sh
    #1

---
+++ hello-world/logs/probot-commands-metadata.sh
@@ -0,0 +1,52 @@
+Microsoft Windows [Version 10.0.18363.1082]
+(c) 2019 Microsoft Corporation. All rights reserved.
+
+C:\Users\droid>cd my-first-app
+
+C:\Users\droid\my-first-app>npm add probot-{commands,metadata}
+npm ERR! code EINVALIDTAGNAME
+npm ERR! Invalid tag name "probot-{commands,metadata}": Tags may not have any characters that encodeURIComponent encodes.
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     C:\Users\droid\AppData\Roaming\npm-cache\_logs\2020-10-07T03_05_10_173Z-debug.log
+
+C:\Users\droid\my-first-app>npm add probot-{commands,metadata}
+npm ERR! code EINVALIDTAGNAME
+npm ERR! Invalid tag name "probot-{commands,metadata}": Tags may not have any characters that encodeURIComponent encodes.
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     C:\Users\droid\AppData\Roaming\npm-cache\_logs\2020-10-07T03_08_16_737Z-debug.log
+
+C:\Users\droid\my-first-app>$ npm add probot-{commands,metadata}
+'$' is not recognized as an internal or external command,
+operable program or batch file.
+
+C:\Users\droid\my-first-app>npm add probot-{commands,metadata}
+npm ERR! code EINVALIDTAGNAME
+npm ERR! Invalid tag name "probot-{commands,metadata}": Tags may not have any characters that encodeURIComponent encodes.
+
+npm ERR! A complete log of this run can be found in:
+npm ERR!     C:\Users\droid\AppData\Roaming\npm-cache\_logs\2020-10-07T03_08_53_436Z-debug.log
+
+C:\Users\droid\my-first-app>cd ../
+
+C:\Users\droid>cd my-first-app
+
+C:\Users\droid\my-first-app>npm add probot-metadata
+npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@2.1.3 (node_modules\fsevents):
+npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.1.3: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
+
++ probot-metadata@1.0.1
+added 1 package from 1 contributor and audited 901 packages in 6.905s
+
+30 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+
+
+C:\Users\droid\my-first-app>npm add probot-commands
+npm WARN optional SKIPPING OPTIONAL DEPENDENCY: fsevents@2.1.3 (node_modules\fsevents):
+npm WARN notsup SKIPPING OPTIONAL DEPENDENCY: Unsupported platform for fsevents@2.1.3: wanted {"os":"darwin","arch":"any"} (current: {"os":"win32","arch":"x64"})
+
+[
